a45a8709651850deb7dee0616bc8820ad75ef4ab
ilawy <next.mohammed.amr@gmail.com>
2025-05-07 21:52:08 +0300

    hotfix: don't display `no results` until loading is done

--- src/routes/Search.tsx
+++ src/routes/Search.tsx
@@ -25,7 +25,7 @@ export default function Search() {
             pb
                 .collection("profiles")
                 .getList(1, 5, { filter: `name~'${search}'` })
-                .then((data) => data.items) as Promise<ProfilesResponse[]>,
+                .then((data) => data.items) as Promise<ProfilesResponse[]>
     );
     const { user } = useUser();
     useEffect(() => {
@@ -37,14 +37,14 @@ export default function Search() {
             }
             getFollowedUsers(
                 `${user?.id}`,
-                result.map((item) => item.id) || [],
+                result.map((item) => item.id) || []
             ).then(setFollows);
         });
     }, [term]);
 
     const [{ loading: followLoading }, invokeFollowUser] = useAsyncFn(
         (...args: Parameters<typeof followUser>) =>
-            followUser(...args).catch(displayError),
+            followUser(...args).catch(displayError)
     );
 
     return (
@@ -81,7 +81,7 @@ export default function Search() {
                 {searchResult &&
                     searchResult.map((item) => {
                         const isFollowed = !!follows.find(
-                            (f) => f.followee === item.id,
+                            (f) => f.followee === item.id
                         );
                         return (
                             <li key={item.id} className="list-row">
@@ -105,7 +105,7 @@ export default function Search() {
                                         onClick={() =>
                                             invokeFollowUser(
                                                 user!.id,
-                                                item.id,
+                                                item.id
                                             ).catch(console.log)
                                         }
                                         className="btn btn-square btn-ghost">
@@ -127,7 +127,9 @@ export default function Search() {
             </ul>
             <div className="flex flex-col justify-center items-center">
                 {term.length < 3 && <p>Please write something to search</p>}
-                {term.length >= 3 && !searchResult?.length && <p>No results</p>}
+                {term.length >= 3 && !searchResult?.length && !loading && (
+                    <p>No results</p>
+                )}
             </div>
         </div>
     );
